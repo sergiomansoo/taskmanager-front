78e4700b856511a3232a113ce467600ebb39b0bc
sergiomansoo <sergio.manso2007@gmail.com>
2026-06-10 22:21:33 -0300

    feat: create new NavBar buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,37 @@
 import { NavLink } from 'react-router-dom'
 import './NavBar.css'
+import { useEffect,useState } from 'react'
+import api from '../api/api'
+
+
 
 function Navbar({ onLogout , usuario }) {
+  const [user,setUser]=useState(null)
+  useEffect(()=>{
+    async function usuarioAtual() {
+      try{
+        const token=localStorage.getItem('token')
+      const res=await api.get('/usuario/me',{headers:{Authorization:`Bearer ${token}`}})
+      setUser(res.data)    }
+    catch(error){
+      
+      console.log("erro")
+    }
+    
+      
+      
+    }
+    usuarioAtual()
+    
+  },[])
+  
   return (
     <nav>
       <NavLink to="/tarefas">Tarefas</NavLink>
-      <NavLink to="/criar-tarefa">Criar tarefa</NavLink>
-      <NavLink to="/usuarios">Usuarios</NavLink>
-      <NavLink  to="/criar-usuario">Criar usuario</NavLink>
+      <NavLink to="/criar-tarefa">Criar Tarefa</NavLink>
+      {user?.role==='ADMIN' && <NavLink to="/criar-tarefa-id-usuario">Criar Tarefa | ID USER</NavLink>}
+      {user?.role==='ADMIN' && <NavLink to="/usuarios">Usuários</NavLink>}
+      {user?.role==='ADMIN' && <NavLink  to="/criar-usuario">Criar Usuário</NavLink>}
 
       <NavLink className={'profile'} to="/usuarios/me">Meu perfil</NavLink>
 
